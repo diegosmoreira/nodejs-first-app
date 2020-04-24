99ef63c1dc42f8eacd3659046d91471474064e96
Diego Soares <soares.diego95@hotmail.com>
2020-04-24 09:30:33 -0300

    Mongo database connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const app = require('./app');
+const mongo = require('mongoose');
 
 app.set('port', 7777);
 
+mongo.connect('mongodb://127.0.0.1:27017/blog', {useUnifiedTopology: true, useNewUrlParser: true});
+
+mongo.Promise = global.Promise;
+mongo.connection.on('error', (error) =>{
+    console.error('ERROR: ' + error.message);
+});
+
 const server = app.listen(app.get('port'), () => {
     console.log('Server running on port: ' + server.address().port);
-})
+});
