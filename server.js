cb361bebf40bcb733261dba123a7ae7b9b9bac68
Diego Soares <soares.diego95@hotmail.com>
2020-04-24 09:53:30 -0300

    Configure variables file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config({path: 'variables.env'});
+
 const app = require('./app');
 const mongo = require('mongoose');
 
-app.set('port', 7777);
+app.set('port', process.env.PORT || 7777);
 
-mongo.connect('mongodb://127.0.0.1:27017/blog', {useUnifiedTopology: true, useNewUrlParser: true});
+mongo.connect(process.env.DATABASE, {useUnifiedTopology: true, useNewUrlParser: true});
 
 mongo.Promise = global.Promise;
 mongo.connection.on('error', (error) =>{
