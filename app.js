8dadf82856380da2cf5dadfacb6be182030b00e3
Diego Soares <soares.diego95@hotmail.com>
2020-04-27 10:33:03 -0300

    Error handler and helpers

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const router = require('./routes/index');
 const mustache = require('mustache-express');
+const helpers = require('./helpers');
+const errorHandler = require('./handlers/errorHandler');
 
+app.use((req, res, next) => {
+    res.locals.h = helpers;
+    next();
+});
 app.use('/', router);
-
 app.use(express.json());
-
 app.engine('mst', mustache(__dirname + '/views/partials', '.mst'));
 app.set('view engine', 'mst');
 app.set('views', __dirname + '/views');
+app.use(errorHandler.notFound);
 
 module.exports = app;
---
+++ helpers.js
@@ -0,0 +1,6 @@
+exports.defaultPageName = 'My Page';
+exports.movies = [
+    {name: 'Star Wars', year: 1977},
+    {name: 'The Lord Of The Rings', year: 2001},
+    {name: 'Avengers', year: 2011}
+];
