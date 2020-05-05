3fcc639dff3cb1de6b0b8858b346c5912479d535
Diego Soares <soares.diego95@hotmail.com>
2020-05-05 11:13:49 -0300

    Login and logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,23 @@ exports.login = (req, res) => {
     res.render('login');
 };
 
+exports.loginAction = (req, res) => {
+    const auth = User.authenticate();
+
+    auth(req.body.email, req.body.password, (err, result) => {
+
+        if(!result){
+            req.flash('error', 'Wrong e-mail or password!');
+            res.redirect('/users/login');
+            return;
+        }
+
+        req.login(result, () => {});
+        req.flash('success', 'Login successufully!');
+        res.redirect('/');
+    });
+};
+
 exports.register = (req, res) => {
     res.render('register');
 };
@@ -13,11 +30,17 @@ exports.registerAction = (req, res) => {
     const newUser = new User(req.body)
     User.register(newUser, req.body.password, (err) =>{
         if(err){
-            console.log('ERROR: ', + err);
+            req.flash('error', 'Occured an error. Try again later.');
             res.redirect('/');
             return;
         }
 
-        res.redirect('/');
+        req.flash('success', 'Successfully registred!. Go to login.');
+        res.redirect('/users/login');
     });
 };
+
+exports.logout = (req, res) => {
+    req.logout();
+    res.redirect('/');
+}
